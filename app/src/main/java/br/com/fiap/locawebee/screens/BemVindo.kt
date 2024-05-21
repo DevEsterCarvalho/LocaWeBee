9b4817f9bf8237f0e579b27c5cb17a0985c654b8
DevEsterCarvalho <estercarvalho205@gmail.com>
2024-05-21 20:15:39 -0300

    finalizando tela bem vindo

--- app/src/main/java/br/com/fiap/locawebee/screens/BemVindo.kt
+++ app/src/main/java/br/com/fiap/locawebee/screens/BemVindo.kt
@@ -1,6 +1,5 @@
 package br.com.fiap.locawebee.screens
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -11,84 +10,140 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.com.fiap.locawebee.R
+import br.com.fiap.locawebee.ui.theme.PoppinsRegular
+import br.com.fiap.locawebee.ui.theme.PoppinsSemiBold
 
 
 @Composable
 fun BemVindo() {
     Box(
         modifier = Modifier
-            .fillMaxSize(), contentAlignment = Alignment.Center
-    ) {
-        Column(
-            verticalArrangement = Arrangement.Center
+            .fillMaxSize()
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.fundo_localwebee),
+            contentDescription = "fundo LocaWeBee",
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.TopCenter)
+        )
+        Column (
+            modifier = Modifier.fillMaxSize()
+                .padding(bottom = 130.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+
         ) {
-            Card(
-                modifier = Modifier.align(Alignment.CenterHorizontally)
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.bee_logo),
-                    contentDescription = "ícone do projeto saúde integrada",
-                    modifier = Modifier.size(width = 191.dp, height = 165.dp),
-                    contentScale = ContentScale.Fit
-                )
-            }
-            Text(text = "LocaWeBEE")
-            Text(text = "Conheça uma forma divertida de gerenciar seu e-mail")
-        }
-        Row(
-            //verticalArrangement = Arrangement.Center
-        ){
-            Button(onClick = {},
-                colors = ButtonDefaults.buttonColors(Color(0xff1F41BB)),
-                modifier = Modifier
-                    .width(142.dp)
-                    .height(45.dp),
-                shape = RoundedCornerShape(6.dp),
-                contentPadding = PaddingValues(5.dp)
+            Image(
+                painter = painterResource(id = R.drawable.bee_logo),
+                contentDescription = "ícone do projeto LocaWeBee",
+                modifier = Modifier.size(width = 191.dp, height = 165.dp)
             )
-            {
-                Text(text = "Entrar",
-                    color = (Color.White),
-                    fontSize = 16.sp,
-                    //fontFamily = InterBold,
-                    textAlign = TextAlign.Center
+        }
+        Column (
+            modifier = Modifier.fillMaxSize()
+            .padding(bottom = 100.dp),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally
+
+        ){
+            Row {
+                Text(
+                    text = "LocaWe",
+                    fontSize = 35.sp,
+                    fontFamily = PoppinsSemiBold,
+                    textAlign = TextAlign.Center,
+                    color = Color (0xFF1F41BB),
+                    modifier = Modifier
+                        .padding(top = 10.dp)
+                )
+                Text(
+                    text = "BEE",
+                    fontSize = 35.sp,
+                    fontFamily = PoppinsSemiBold,
+                    textAlign = TextAlign.Center,
+                    color = Color(0xFFED0000),
+                    modifier = Modifier
+                        .padding(top = 10.dp)
                 )
             }
             Spacer(modifier = Modifier.width(16.dp))
-            Button(onClick = {},
-                border = BorderStroke(1.dp, (Color.White)),
-                colors = ButtonDefaults.buttonColors(Color.White),
+            Text(
+                text = "Conheça uma forma divertida de\n gerenciar seu e-mail",
+                fontSize = 14.sp,
+                fontFamily = PoppinsRegular,
+                textAlign = TextAlign.Center,
+                color = Color.Black,
                 modifier = Modifier
-                    .width(142.dp)
-                    .height(45.dp),
-                shape = RoundedCornerShape(6.dp),
+                    .fillMaxWidth()
+                    .padding(top = 10.dp, bottom = 50.dp)
+            )
+        }
+        Box(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 50.dp)
+        ){
+            Row(
+                modifier = Modifier
+                    .padding(10.dp)
 
+            ){
+                Button(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(Color(0xff1F41BB)),
+                    modifier = Modifier
+                        .width(144.dp)
+                        .height(44.dp),
+                    shape = RoundedCornerShape(10.dp),
+                    contentPadding = PaddingValues(5.dp),
+                    elevation = ButtonDefaults.buttonElevation(3.dp)
                 ){
-                Text(text = "Cadastrar",
-                    color = (Color(0xff0A0A0A)),
-                    fontSize = 16.sp,
-                    //fontFamily = InterBold,
-                    textAlign = TextAlign.Center
-                )
+                    Text(
+                        text = "Entrar",
+                        color = (Color.White),
+                        fontSize = 16.sp,
+                        fontFamily = PoppinsSemiBold,
+                        textAlign = TextAlign.Center
+                    )
+                }
+                Spacer(modifier = Modifier.width(16.dp))
+                Button(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(Color.White),
+                    modifier = Modifier
+                        .width(144.dp)
+                        .height(44.dp),
+                    shape = RoundedCornerShape(10.dp),
+                    contentPadding = PaddingValues(5.dp),
+                    elevation = ButtonDefaults.buttonElevation(3.dp)
+                ){
+                    Text(
+                        text = "Cadastrar",
+                        color = (Color(0xff0A0A0A)),
+                        fontSize = 16.sp,
+                        fontFamily = PoppinsSemiBold,
+                        textAlign = TextAlign.Center
+                    )
+                }
             }
         }
     }
@@ -98,9 +153,9 @@ fun BemVindo() {
 @Preview(showBackground = true)
 @Composable
 fun BemVindoPreview() {
-    Surface (modifier = Modifier.fillMaxWidth()
+    Surface(
+        modifier = Modifier.fillMaxWidth()
     ) {
         BemVindo()
     }
 }
-
--- app/src/main/java/br/com/fiap/locawebee/ui/theme/Type.kt
+++ app/src/main/java/br/com/fiap/locawebee/ui/theme/Type.kt
@@ -2,9 +2,18 @@ package br.com.fiap.locawebee.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import br.com.fiap.locawebee.R
+
+val PoppinsBold = FontFamily(Font(R.font.poppins_bold))
+val PoppinsRegular = FontFamily(Font(R.font.poppins_regular))
+val PoppinsSemiBold = FontFamily(Font(R.font.poppins_semibold))
+val PoppinsMedium = FontFamily(Font(R.font.poppins_medium))
+val OpenSansRegular = FontFamily(Font(R.font.opensans_regular))
+val Roboto = FontFamily(Font(R.font.roboto_regular))
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -15,6 +24,7 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
