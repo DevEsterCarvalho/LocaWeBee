464748f6033697827dd24c725f459143d25d9c1b
DevEsterCarvalho <estercarvalho205@gmail.com>
2024-05-28 20:11:30 -0300

    editando screens

---
+++ app/src/main/java/br/com/fiap/locawebee/screens/PrimeiroAcesso.kt
@@ -0,0 +1,323 @@
+package br.com.fiap.locawebee.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.com.fiap.locawebee.R
+import br.com.fiap.locawebee.ui.theme.PoppinsMedium
+import br.com.fiap.locawebee.ui.theme.PoppinsSemiBold
+
+@Composable
+fun PrimeiroAcesso() {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.White)
+    ){
+        Box(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .fillMaxWidth()
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.fundo_premio),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize()
+            )
+            BoxWithConstraints(
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .height(328.dp)
+                    .width(329.dp)
+                    .background(Color(0xffF9F9F9), shape = RoundedCornerShape(13.dp))
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.medalha),
+                    contentDescription = "imagem medalha premio",
+                    modifier = Modifier
+                        .size(width = 132.dp, height = 132.dp)
+                        .align(Alignment.TopCenter)
+                        .offset(y = (-80).dp)
+                )
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 20.dp)
+                ) {
+                    Text(
+                        text = "Prêmio Diário\n" +
+                                "Concedido!",
+                        fontSize = 32.sp,
+                        fontFamily = PoppinsSemiBold,
+                        textAlign = TextAlign.Center,
+                        color = Color(0xff1D1F33),
+                        modifier = Modifier.padding(top = 50.dp)
+                    )
+                    Spacer(modifier = Modifier.height(10.dp))
+                    Text(
+                        text = "Temos um presente para você\n" +
+                                "pelo seu primeiro acesso!\n" +
+                                "Use com sabedoria!",
+                        fontSize = 16.sp,
+                        fontFamily = PoppinsMedium,
+                        textAlign = TextAlign.Center,
+                        color = Color(0xff1D1F33),
+                        modifier = Modifier.padding(bottom = 30.dp)
+                    )
+                }
+                Button(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(Color(0xff1F41BB)),
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .width(220.dp)
+                        .height(31.dp)
+                        .offset(y = 10.dp),
+                    shape = RoundedCornerShape(10.dp),
+                    contentPadding = PaddingValues(5.dp),
+                    elevation = ButtonDefaults.buttonElevation(25.dp)
+                ) {
+                    Text(
+                        text = "Obrigado!",
+                        color = Color.White,
+                        fontSize = 16.sp,
+                        fontFamily = PoppinsSemiBold,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+        }
+
+        Box (
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(top = 650.dp)
+                .background(Color(0xff1D1F33))
+                .border(1.dp, Color(0xffFFFFFF))
+        ){
+            Row (
+                horizontalArrangement = Arrangement.spacedBy(10.dp, Alignment.CenterHorizontally),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .width(380.dp)
+                    .height(58.dp)
+            ){
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(Color(0xff1D1F33)),
+                    contentPadding = PaddingValues(5.dp),
+                    modifier = Modifier
+                        .size(48.dp)
+                        .shadow(10.dp, shape = RoundedCornerShape(4.dp), clip = true)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.pasta_icon),
+                        contentDescription = "ícone pastas categorias email",
+                        modifier = Modifier
+                            .size(width = 29.dp, height = 29.dp)
+                            .padding(1.dp)
+                    )
+                }
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(Color(0xff1D1F33)),
+                    contentPadding = PaddingValues(5.dp),
+                    modifier = Modifier
+                        .size(48.dp)
+                        .shadow(10.dp, shape = RoundedCornerShape(4.dp), clip = true)
+
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.pasta_icon),
+                        contentDescription = "ícone pastas categorias email",
+                        modifier = Modifier
+                            .size(width = 29.dp, height = 29.dp)
+                            .padding(1.dp)
+                    )
+                }
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(Color(0xff1D1F33)),
+                    contentPadding = PaddingValues(5.dp),
+                    modifier = Modifier
+                        .size(48.dp)
+                        .shadow(10.dp, shape = RoundedCornerShape(4.dp), clip = true)
+
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.pasta_icon),
+                        contentDescription = "ícone pastas categorias email",
+                        modifier = Modifier
+                            .size(width = 29.dp, height = 29.dp)
+                            .padding(1.dp)
+                    )
+                }
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(Color(0xff1D1F33)),
+                    contentPadding = PaddingValues(5.dp),
+                    modifier = Modifier
+                        .size(48.dp)
+                        .shadow(10.dp, shape = RoundedCornerShape(4.dp), clip = true)
+
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.pasta_icon),
+                        contentDescription = "ícone pastas categorias email",
+                        modifier = Modifier
+                            .size(width = 29.dp, height = 29.dp)
+                            .padding(1.dp)
+                    )
+                }
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(Color(0xff1D1F33)),
+                    contentPadding = PaddingValues(5.dp),
+                    modifier = Modifier
+                        .size(48.dp)
+                        .shadow(10.dp, shape = RoundedCornerShape(4.dp), clip = true)
+
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.pasta_icon),
+                        contentDescription = "ícone pastas categorias email",
+                        modifier = Modifier
+                            .size(width = 29.dp, height = 29.dp)
+                            .padding(1.dp)
+                    )
+                }
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(Color(0xff1D1F33)),
+                    contentPadding = PaddingValues(5.dp),
+                    modifier = Modifier
+                        .size(48.dp)
+                        .shadow(10.dp, shape = RoundedCornerShape(4.dp), clip = true)
+
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.pasta_icon),
+                        contentDescription = "ícone pastas categorias email",
+                        modifier = Modifier
+                            .size(width = 29.dp, height = 29.dp)
+                            .padding(1.dp)
+                    )
+                }
+            }
+        }
+        Box (
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .background(Color(0xff1D1F33))
+                .border(1.dp, Color(0xffFFFFFF)),
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(40.dp, Alignment.CenterHorizontally),
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .width(375.dp)
+                    .height(76.dp)
+            ) {
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(Color(0xff1D1F33)),
+                    contentPadding = PaddingValues(5.dp),
+                    modifier = Modifier
+                        .size(48.dp)
+                        .shadow(10.dp, shape = RoundedCornerShape(4.dp), clip = true)
+
+                ) {
+                    Icon(imageVector = Icons.Default.Settings , contentDescription = "ícone settings",
+                        tint = Color.White, modifier = Modifier.size(45.dp))
+                }
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(Color(0xff1D1F33)),
+                    contentPadding = PaddingValues(5.dp),
+                    modifier = Modifier
+                        .size(48.dp)
+                        .shadow(10.dp, shape = RoundedCornerShape(4.dp), clip = true)
+
+                ) {
+                    Icon(imageVector = Icons.Default.Search, contentDescription = "ícone pesquisa",
+                        tint = Color.White, modifier = Modifier.size(45.dp))
+                }
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(Color(0xff1D1F33)),
+                    contentPadding = PaddingValues(5.dp),
+                    modifier = Modifier
+                        .size(48.dp)
+                        .shadow(10.dp, shape = RoundedCornerShape(4.dp), clip = true)
+
+                ) {
+                    Icon(imageVector = Icons.Default.DateRange, contentDescription = "ícone calendário",
+                        tint = Color.White, modifier = Modifier.size(45.dp))
+                }
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(Color(0xff1D1F33)),
+                    contentPadding = PaddingValues(5.dp),
+                    modifier = Modifier
+                        .size(48.dp)
+                        .shadow(10.dp, shape = RoundedCornerShape(4.dp), clip = true)
+
+                ) {
+                    Icon(imageVector = Icons.Default.Star, contentDescription = "ícone favoritos",
+                        tint = Color(color = 0xffFBBC24), modifier = Modifier.size(45.dp))
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun PrimeiroAcessoPreview() {
+    Surface(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        PrimeiroAcesso()
+    }
+}
